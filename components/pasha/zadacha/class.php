e4f3f1485ac8a23dd5c3f30d94e4a70cb877409b
Lapulay <pasha.terminator007@mail.ru>
2022-07-28 11:44:02 +0300

    Переменные приведены к формату, выровнены отступы, в конце функций добавлен return

--- components/pasha/zadacha/class.php
+++ components/pasha/zadacha/class.php
@@ -1,16 +1,17 @@
 <?php
-if(!defined("B_PROLOG_INCLUDED")|| B_PROLOG_INCLUDED!==true)die();
-class CAddzadacha extends CBitrixComponent
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
+class CAddzadacha extends CBitrixComponent
 {
-    public  $arEventFields = [];
+    public $arEventFields = [];
     public $componentPage = "";
 
     function displayTemplate()
     {
 
         CModule::IncludeModule('iblock');
-        $obResult = CIBlockElement::GetList([], ['IBLOCK_ID' => 1], false, false, ['ID', 'NAME', 'PROPERTY_OPISANIE', 'PROPERTY_SROK', 'PROPERTY_STATUS']);
+        $obResult = CIBlockElement::GetList([], ['IBLOCK_ID' => 1], false, false,
+            ['ID', 'NAME', 'PROPERTY_OPISANIE', 'PROPERTY_SROK', 'PROPERTY_STATUS']);
 
         $arTasks = [];
         while ($arElement = $obResult->fetch()) {
@@ -25,81 +26,74 @@ class CAddzadacha extends CBitrixComponent
         $arProperties = [];
         $obEnums = CIBlockPropertyEnum::GetList(array("DEF" => "DESC", "SORT" => "ASC"), array("IBLOCK_ID" => 1));
         while ($arEnum = $obEnums->GetNext()) {
-            $arProperties[$arEnum['ID']]=['VALUE'=>$arEnum['VALUE'], 'CODE'=>$arEnum['XML_ID']];
+            $arProperties[$arEnum['ID']] = ['VALUE' => $arEnum['VALUE'], 'CODE' => $arEnum['XML_ID']];
             [$arEnum['ID']] = $arEnum;
-            [$arEnum['ID']]=$arEnum['VALUE'];
+            [$arEnum['ID']] = $arEnum['VALUE'];
         }
-        $this->arResult['LIST_VALUES']=$arProperties;
-
+        return $arProperties;
     }
-
 
     public function deleteElement($id)
     {
         CIBlockElement::Delete($id);
-
+        return null;
     }
-    function accessUser(){
-        $arRoles=['ACTIVE'=>"", 'AVAILABLE_STATUS'=>[]];
 
+    function accessUser()
+    {
+        $arRoles = ['ACTIVE' => "", 'AVAILABLE_STATUS' => []];
         global $USER;
         $arGroups = $USER->GetUserGroupArray();
-        $sGroupsSep=implode("|", $arGroups);
+        $sGroupsSep = implode("|", $arGroups);
+        $arCodeGroup = CGroup::GetList($by = "c_sort", $order = "asc", array("ID" => $sGroupsSep));
 
-
-      $arCodeGroup = CGroup::GetList($by = "c_sort", $order = "asc", array("ID" => $sGroupsSep));
-
-       while ($arEnum = $arCodeGroup ->GetNext()) {
-          $arTest[$arEnum['ID']]=$arEnum['STRING_ID'];
-
-
-      }
-
+        while ($arEnum = $arCodeGroup->GetNext()) {
+            $arTest[$arEnum['ID']] = $arEnum['STRING_ID'];
+        }
 
         if (in_array("KURATOR", $arTest) || in_array("ADMIN", $arTest)) {
-            $arRoles['ACTIVE']="KURATOR";
-            $arRoles['AVAILABLE_STATUS']=["ZAVERSHENA", "OTMENENA"];
+            $arRoles['ACTIVE'] = "KURATOR";
+            $arRoles['AVAILABLE_STATUS'] = ["ZAVERSHENA", "OTMENENA"];
         }
         if (in_array("PODOPECHNIY", $arTest)) {
-            $arRoles['ACTIVE']="PODOPECHNIY";
-            $arRoles['AVAILABLE_STATUS']=["COMPLITED", "REJECTED", "PERFORMED"];
+            $arRoles['ACTIVE'] = "PODOPECHNIY";
+            $arRoles['AVAILABLE_STATUS'] = ["COMPLITED", "REJECTED", "PERFORMED"];
         }
-        $this->arResult['ROLE']=$arRoles;
-
+        $this->arResult['ROLE'] = $arRoles;
+        return null;
     }
 
     function createElement()
     {
         $obEl = new CIBlockElement;
-        $PROP = array();
-        $PROP['NAME'] = $_POST["NAME"];
-        $PROP['OPISANIE'] = $_POST['OPISANIE'];
-        $PROP['SROK'] = $_POST['SROK'];
-        $PROP['STATUS'] = $_POST['STATUS'];
+        $arProp = array();
+        $arProp['NAME'] = $_POST["NAME"];
+        $arProp['OPISANIE'] = $_POST['OPISANIE'];
+        $arProp['SROK'] = $_POST['SROK'];
+        $arProp['STATUS'] = $_POST['STATUS'];
         $arLoadProductArray = array(
             'IBLOCK_ID' => 1,
             "NAME" => $_POST["NAME"],
             "ID" => $_POST["ID"],
-            "PROPERTY_VALUES" => $PROP,
+            "PROPERTY_VALUES" => $arProp,
 
         );
-        $obEl->Add($arLoadProductArray);
-
+        return $obEl->Add($arLoadProductArray);
     }
 
     function editingStatus($statusID, $elementId)
     {
         CModule::IncludeModule('iblock');
 
-        $PROPERTY_CODE = "STATUS";
-        $PROPERTY_VALUE = $statusID;
+        $sPropertyCode = "STATUS";
+        $iPropertyValue = $statusID;
 
-        CIBlockElement::SetPropertyValuesEx($elementId, false, array($PROPERTY_CODE => $PROPERTY_VALUE));
-
+        CIBlockElement::SetPropertyValuesEx($elementId, false, array($sPropertyCode =>  $iPropertyValue));
+        return null;
     }
 
-
-    function mailerByAdd(){
+    function mailerByAdd()
+    {
         $this->arEventFields = array(
             'IBLOCK_ID' => 1,
             "NAME" => ($_POST["NAME"]),
@@ -107,16 +101,19 @@ class CAddzadacha extends CBitrixComponent
             "SROK" => ($_POST["SROK"]),
             "STATUS" => ($_POST["STATUS"]),
         );
-        CEvent::Send('MAIL_NOTIFICATION',  's1', $this->arEventFields, 'N', '11',array());
+        CEvent::Send('MAIL_NOTIFICATION', 's1', $this->arEventFields, 'N', '11', array());
+        return null;
     }
 
-    function mailerByModyfy(){
+    function mailerByModyfy()
+    {
         $this->arEventFields = array(
             'IBLOCK_ID' => 1,
             'NAME' => ($_POST["TASK"]),
             "STATUS" => ($_POST["STATUS"]),
         );
-        CEvent::Send('MAIL_NOTIFICATION',  's1', $this->arEventFields, 'N', '12',array());
+        CEvent::Send('MAIL_NOTIFICATION', 's1', $this->arEventFields, 'N', '12', array());
+        return null;
     }
 
     function makeRequests()
@@ -130,28 +127,27 @@ class CAddzadacha extends CBitrixComponent
         if (isset($_REQUEST['ID'])) {
             $this->deleteElement($_REQUEST['ID']);
         }
-        if(!empty($_REQUEST["iblock_status"])){
+        if (!empty($_REQUEST["iblock_status"])) {
             $this->componentPage = "template_status";
         }
-        if (isset($_POST['STATUS'])&&$_POST['form_id']=='Add_status'){
+        if (isset($_POST['STATUS']) && $_POST['form_id'] == 'Add_status') {
             $this->editingStatus($_POST['STATUS'], $_POST['TASK']);
             $this->mailerByModyfy();
         }
-        if ($_POST['form_id']=='Add_Item'){
+        if ($_POST['form_id'] == 'Add_Item') {
             $this->createElement();
             $this->mailerByAdd();
         }
+        return null;
     }
 
     function executeComponent()
     {
+        $this->arResult['LIST_VALUES'] =  $this->displayTemplate();
         $this->accessUser();
         $this->displayTemplate();
         $this->makeRequests();
-       // $this->createElement();
-//        $this->delete($name);
         $this->includeComponentTemplate($this->componentPage);
-
     }
 }
 
--- components/pasha/zadacha/templates/.default/template.php
+++ components/pasha/zadacha/templates/.default/template.php
@@ -1,47 +1,48 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED")|| B_PROLOG_INCLUDED!== true)die();
-
 ?>
 
 <?php
-
-$this->addExternalCss("/local/css/bootstrap.min.css");
-$this->addExternalJS("/local/js/bootstrap.bundle.min.js");
+    $this->addExternalCss("/local/css/bootstrap.min.css");
+    $this->addExternalJS("/local/js/bootstrap.bundle.min.js");
 ?>
-
-
-<script src="bootstrap.bundle.min.js"></script>
-
 
 <table class="table  table-striped table-bordered" >
     <thead>
-    <tr>
-        <th>Название</th>
-        <th>Описание</th>
-        <th>Крайний срок</th>
-        <th>Статус</th>
-    </tr>
+        <tr>
+            <th>Название</th>
+            <th>Описание</th>
+            <th>Крайний срок</th>
+            <th>Статус</th>
+        </tr>
     </thead>
 
     <? foreach ($arResult['TASKS'] as  $key=>$arTask): ?>
-    <tr>
-        <td><?= $arTask['NAME']?></td>
-        <td><?= $arTask['OPISANIE']?></td>
-        <td><?= $arTask['SROK']?></td>
-        <td>
-            <?= $arTask['STATUS']?></td>
-        <? if ($arResult['ROLE']['ACTIVE']=="KURATOR"):?>
-        <td><a href="?ID=<?=$key?> "class="btn btn-danger" >Удалить</td>
-        <? endif;?>
-
-
-    </tr>
-
+        <tr>
+            <td>
+                <?= $arTask['NAME']?>
+            </td>
+            <td>
+                <?= $arTask['OPISANIE']?>
+            </td>
+            <td>
+                <?= $arTask['SROK']?>
+            </td>
+            <td>
+                <?= $arTask['STATUS']?>
+            </td>
+            <? if ($arResult['ROLE']['ACTIVE']=="KURATOR"):?>
+                <td>
+                    <a href="?ID=<?=$key?> "class="btn btn-danger" >Удалить
+                </td>
+            <? endif;?>
+        </tr>
     <?endforeach; ?>
 </table>
-<? if ($arResult['ROLE']['ACTIVE']=="KURATOR"):?>
-<a href="?iblock_Add=Y"  class="btn btn-primary">добавить</a>
-<? endif;?>
 
-<a href="?iblock_status=Y" class="btn btn-primary">Изменить статус</a>
+    <? if ($arResult['ROLE']['ACTIVE']=="KURATOR"):?>
+        <a href="?iblock_Add=Y"  class="btn btn-primary">добавить</a>
+    <? endif;?>
+
+    <a href="?iblock_status=Y" class="btn btn-primary">Изменить статус</a>
 
